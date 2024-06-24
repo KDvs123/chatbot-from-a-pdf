07640c9b83a7b8d798af0c16430c3163592c5174
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-06-24 23:34:45 +0530

    implemented the generating result method

--- app.py
+++ app.py
@@ -3,11 +3,16 @@ from langchain import InMemoryDocstore
 import streamlit as st
 import faiss
 from dotenv import load_dotenv
+import numpy as np
 
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+
+from langchain.callbacks import get_openai_callback
 
 
 st.title('Chatbot')
@@ -44,6 +49,17 @@ def main():
         
         store_name = pdf.name[:-4]
         index_folder = f'src/faiss_store/{store_name}'
+      
+        embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
+
+        embedding_dim=1536
+        faiss_index = faiss.IndexFlatL2(embedding_dim)
+        # Initialize the docstore
+        docstore = InMemoryDocstore()
+        # Initialize the index_to_docstore_id
+        index_to_docstore_id = {}
+        # vectorstores = FAISS(chunks,embeddings)
+        vectorstores = FAISS(embedding_function=embeddings, index=faiss_index, docstore=docstore, index_to_docstore_id=index_to_docstore_id)
 
         if os.path.exists(index_folder):
             try:
@@ -53,26 +69,39 @@ def main():
                 st.write(f"Failed to load local storage: {e}")
         else:
             try: 
-                embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
-                embedding_dim=1024
-                faiss_index = faiss.IndexFlatL2(embedding_dim)
-                # Initialize the docstore
-                docstore = InMemoryDocstore()
-                # Initialize the index_to_docstore_id
-                index_to_docstore_id = {}
-                # vectorstores = FAISS(chunks,embeddings)
-                vectorstores = FAISS(embedding_function=embeddings, index=faiss_index, docstore=docstore, index_to_docstore_id=index_to_docstore_id)
+                
                 vectorstores.save_local(index_folder)
                 # st.write("Saved vectorstores to local storage")
             except Exception as e:
-                st.write(f"Failed to save local storage: {e}")
+               st.write(f"Failed to save local storage: {e}")
+
+        
+        query= st.text_input("Ask questions about your pdf file")
+
+
+        if query:
+            
+            docs = vectorstores.similarity_search(query, k=3)
+ 
+            llm = OpenAI()
+            chain = load_qa_chain(llm=llm, chain_type="stuff")
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents=docs, question=query)
+                print(cb)
+            st.write(response)
+ 
+
+
+
+
+           
+
+
 
             
-            # Accept user questions/ query
 
-            query=st.text_input("Ask Questions about the PDF:")
-            st.write(query)
             
+          
 
         
 
